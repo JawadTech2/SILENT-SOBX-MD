9670404854ba3d573d15561c9f513e31f9251d59
JawadTech2 <jawadbahix@outlook.com>
2024-11-22 13:28:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+    return
+        lrktext === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "bjZXDJhQ#fptxXWHUQk8mddIr0x24Ii72KXK1hLyl5JRbe9PJNA0",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
